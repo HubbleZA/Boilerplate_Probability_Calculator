72ceb5739b96a01605ff940bb689209e630bdbb7
HubbleZA <rladeira95@gmail.com>
2022-01-18 22:18:13 +0200

    Upload

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,10 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self):
+        return None
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+
+    return None
