30092f9cd713d00b9cc46e75a48e8349118ae720
HubbleZA <rladeira95@gmail.com>
2022-01-23 01:13:33 +0200

    Starting to Code

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,8 +3,15 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    def __init__(self):
-        return None
+    def __init__(self, **kwargs):
+        balls = []
+        for i in kwargs:
+            k = 1
+            while k <= kwargs[i]:
+                balls.append(i)
+                k += 1
+        print(balls)
+        print(kwargs)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
